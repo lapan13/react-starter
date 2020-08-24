938bf37c8681a8f299f5873227b07e27f50f5524
lapan13 <ariirwandi13@gmail.com>
2020-08-24 14:38:41 +0700

    commit all files starter kit

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+
+import style from "./app.css";
+
+const App = () => {
+    return <div className={style.app}>react Starter 🚀 
+        <h1>Assalamu'alaikum</h1>
+    </div>;
+};
+
+export default hot(module)(App);
